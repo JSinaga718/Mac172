284f562ab1c0936918030d80c77eb3ece690c2b1
JSinaga718 <55105912+JSinaga718@users.noreply.github.com>
2021-12-07 11:35:16 -0500

    5th commit

--- weWearStreetWear/myjs.js
+++ weWearStreetWear/myjs.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Dec 31, 2021 00:00:25").getTime();
+var countDownDate = new Date("Dec 25, 2021 00:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -16,7 +16,7 @@ var x = setInterval(function() {
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
     
-  // Output the result in an element with id="demo"
+  // Output 
   document.getElementById("demo").innerHTML = days + "d " + hours + "h "
   + minutes + "m " + seconds + "s ";
     
